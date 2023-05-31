5fd0433032b674453d0b01ff3d275c0888eab416
BlueNoul <bb7788@naver.com>
2023-06-01 02:15:37 +0900

    로그인 기초

--- login/register.js
+++
@@ -1,9 +0,0 @@
-/* Form validation */
-function validateForm() {
-    var username = document.forms["myForm"]["username"].value;
-    var password = document.forms["myForm"]["password"].value;
-    if (username == "" || password == "") {
-        alert("Both fields must be filled out");
-        return false;
-    }
-}
