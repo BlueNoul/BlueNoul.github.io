6e18819a38b02027dbe625a9910f70615a42f1df
BlueNoul <bb7788@naver.com>
2023-06-01 03:38:30 +0900

    dd

--- login/sign_pros.php
+++ login/sign_pros.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect(주소, 아이디, "비밀번호", DB 스키마 이름, 포트);
+$conn = mysqli_connect("202.31.247.183", "root", "byby1004", "user");
 $hashedPassword = password_hash($_POST['passwd'], PASSWORD_DEFAULT);
 echo $hashedPassword;
 $sql = "
