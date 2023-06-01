1f6f63311812493f53ea1de617a3d155e6e1827f
BlueNoul <bb7788@naver.com>
2023-06-01 09:52:26 +0900

    dfd

--- login/sign_in.php
+++ login/sign_in.php
@@ -4,23 +4,27 @@
 $id = $_POST['id'];
 $password = $_POST['pw'];
 
-echo "<h1>sfesf</h1>";
 $conn = mysqli_connect('localhost', 'root', '1234', 'user');
-ysqli_set_charset($conn, 'utf8');  //인코딩 utf8로 설정
+mysqli_set_charset($conn, 'utf8');  //인코딩 utf8로 설정
 
+echo "<h1>t1<br></h1>";
 
 // DB 정보 가져오기 
 $sql = "SELECT * FROM user where id ='$id' && password = '$password';";
 $result = mysqli_query($conn, $sql);
+echo "<h1>t2<br></h1>";
 
 
 $list = mysqli_num_rows($result);
 
+echo "<h1>t3<br></h1>";
 $arr = mysqli_fetch_array($result);
+echo "<h1>t4<br></h1>";
 
 echo "<br><br>";
 if($list) { //쿼리로 받아온 리스트에 요청한 사용자 정보가 있다면 로그인
     session_start();
+    echo "<h1>t5<br></h1>";
     $_SESSION['id'] = $id;
     $_SESSION['password'] = $password;
     
