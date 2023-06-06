9df2df1588cc059306c9dfda9848f1680838e253
BlueNoul <bb7788@naver.com>
2023-06-06 20:47:15 +0900

    d

--- login/sign_up.php
+++ login/sign_up.php
@@ -11,7 +11,6 @@ mysqli_set_charset($conn, 'utf8');  //인코딩 utf8로 설정
 $sql_id = "SELECT * FROM user where id ='$id' ;";//user 테이블의 아이디에서 해당 아이디가 존재한다면
 $id_num= mysqli_num_rows(mysqli_query($conn,$sql_id));// 아이디수
 
-echo("<script>alert("1")</script>");
 
 
 if($id_num) { //쿼리 실행해여 아이디가 있다면
@@ -22,10 +21,11 @@ if($id_num) { //쿼리 실행해여 아이디가 있다면
 else{// 아이디가 없다면 회원가입을 진행함
     //아이디 비번 레코드에 추가
 
-    mysqli_query($conn,"insert into user (id,pw,created) values('"$id"','"$password"','NOW()');");
+    //mysqli_query($conn,"insert into user (id,pw,created) values('"$id"','"$password"','NOW()');");
 
     //
-    echo(json_encode(array('result' => "true", 'id'=>$id)));
+    $json =json_encode(array('result' => "true", 'id'=>$id));
+    echo($json);
     
     session_start();//세션에 아이디와 비밀번호를 저장
     $_SESSION['id']=$id;
